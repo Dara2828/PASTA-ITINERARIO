fb4ce1f07265ca970a7fe8e429d3785ca1f21323
Dara2828 <dhandara.almeida@aluno.fiema.org.br>
2025-09-25 14:39:56 -0300

    questao 2

---
+++ LISTA 1409/questao2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void maior_menor() {
+    int vetor[15], maior, menor;
+    for(int i=0; i<15; i++) {
+        printf("Digite o %dº numero: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+    maior = menor = vetor[0];
+    for(int i=1; i<15; i++) {
+        if(vetor[i] > maior) maior = vetor[i];
+        if(vetor[i] < menor) menor = vetor[i];
+    }
+    printf("Maior: %d | Menor: %d\n", maior, menor);
+}
