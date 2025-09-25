29ea0cd7f83dc5172f7058d36fe7e1a434942779
Dara2828 <dhandara.almeida@aluno.fiema.org.br>
2025-09-25 14:41:15 -0300

    questao 4

---
+++ LISTA 1409/questao4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void pares_impares() {
+    int vetor[20], pares=0, impares=0;
+    for(int i=0; i<20; i++) {
+        printf("Digite o %dº numero: ", i+1);
+        scanf("%d", &vetor[i]);
+        if(vetor[i] % 2 == 0) pares++;
+        else impares++;
+    }
+    printf("Pares: %d | Impares: %d\n", pares, impares);
+}
