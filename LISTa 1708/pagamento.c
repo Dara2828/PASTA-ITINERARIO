cf59db4e2c025f5affeb3e87be02b8c71dba3a0d
Dara2828 <dhandara.almeida@aluno.fiema.org.br>
2025-09-25 14:55:46 -0300

    pagamento

---
+++ LISTa 1708/pagamento.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() { 
+char nome[] = "DHANDARA"; 
+double valorHora = 25.50;  
+double pagamento = valorHora * horas;
+printf("Funcionario(a): %s\n", nome); 
+printf("Valor por hora: %.2lf\n", valorHora); 
+printf("Horas trabalhadas: %d\n", horas); 
+printf("O pagamento para %s deve ser %.2lf\n", nome, pagamento); 
+return 0;
+}
