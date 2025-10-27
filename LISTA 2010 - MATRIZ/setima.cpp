67664e42c80c550b6c074490b0e1f9029b4ab5f1
Dara2828 <dhandara.almeida@aluno.fiema.org.br>
2025-10-27 17:45:43 -0300

    Create setima.cpp

---
+++ LISTA 2010 - MATRIZ/setima.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matriz[2][2];
+    cout << "Digite 4 números para preencher a matriz 2x2:\n";
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            cin >> matriz[i][j];
+
+    cout << "Matriz completa:\n";
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++)
+            cout << matriz[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}
